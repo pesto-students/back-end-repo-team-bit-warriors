c9994754cab9118a4f0afc888a29e9628c8dc96e
hirenjethva <hiren.j@somaiya.edu>
2023-08-01 00:47:27 +0530

    admin routes created

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const usercontroller = require('../controller/users.controller');
 
 router.post('/register', usercontroller.userRegister);
-router.post('/login', usercontroller.userauth);
+router.post('/login', usercontroller.userAuth);
+router.post('/admin/register', usercontroller.adminRegister);
+router.post('/admin/login', usercontroller.adminAuth);
 
 module.exports = router;
