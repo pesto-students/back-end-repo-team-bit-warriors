b0a78c78aa8aee6002cde63a56cb0ec8a12d46af
Mamta Gupta <mamtag962000@gmail.com>
2023-08-05 00:04:03 +0530

    User and Admin creation and logging updated

--- src/controller/auth/adminAuthController.js
+++ src/controller/auth/adminAuthController.js
@@ -2,40 +2,8 @@ require('dotenv').config()
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { Auth } = require('../../model/Auth');
-
-
-// const userRegister = async (req, res) => {
-//     const schema = Joi.object({
-//         username: Joi.string().min(5).max(50).required(),
-//         email: Joi.string().min(5).max(255).required().email(),
-//         password: Joi.string().min(5).max(255).required()
-//     });
-
-//     const { error } = schema.validate(req.body);
-//     if(error) return res.status(400).send('bad request');
-
-//     let user = await User.findOne({email: req.body.email});
-//     if(user) return res.status(400).send('User already registered.');
-
-//     user = new User ({
-//         username : req.body.username,
-//         email : req.body.email,
-//         password : req.body.password
-//     });
-
-//     const salt = await bcrypt.genSalt(10);
-//     user.password = await bcrypt.hash(user.password, salt);
-
-//     await user.save();
-
-//     const register_token = jwt.sign({_id: user._id, isAdmin: user.isAdmin}, process.env.jwtPrivatekey, { expiresIn: '600s' });
-    
-//     res.header('x-auth-token', register_token).send({
-//         username: user.username,
-//         email: user.email
-//     });
-// };
+const Auth = require('../../model/Auth');
+const Admin = require('../../model/Admin');
 
 const register = async (req, res) => {
     const schema = Joi.object({
@@ -64,31 +32,18 @@ const register = async (req, res) => {
 
     const register_token = jwt.sign({_id: user._id, isAdmin: user.isAdmin}, process.env.jwtPrivatekey, { expiresIn: '600s' });
     
+    await Admin.create({
+        username : req.body.username,
+        email : req.body.email,
+        password : req.body.password,
+        joinedAt: new Date()
+    })
+    
     res.header('x-auth-token', register_token).send({
         username: user.username,
         email: user.email
     });
 };
-
-// const userAuth =  async (req, res) => {
-//     const schema = Joi.object({
-//         email: Joi.string().min(5).max(255).required().email(),
-//         password: Joi.string().min(5).max(255).required()
-//     });
-
-//     const { error } = schema.validate(req.body);
-//     if(error) return res.status(400).send('bad request');
-
-//     let user = await User.findOne({email: req.body.email});
-//     if(!user) return res.status(400).send('Invalid email or password.');
-
-//     const validatePassword = await bcrypt.compare(req.body.password, user.password);  
-//     if(!validatePassword) return res.status(400).send('Invalid email or password.');
-
-//     const token = jwt.sign({_id: user._id, isAdmin: user.isAdmin}, process.env.jwtPrivatekey, { expiresIn: '600s' });
-
-//     res.header('x-auth-token', token).send(`Authntication Successful.`);
-// };
 
 const login =  async (req, res) => {
     const schema = Joi.object({
@@ -106,6 +61,14 @@ const login =  async (req, res) => {
     if(!validatePassword) return res.status(400).send('Invalid email or password.');
 
     const token = jwt.sign({_id: user._id, isAdmin: user.isAdmin}, process.env.jwtPrivatekey, { expiresIn: '600s' });
+
+    await Admin.findOneAndUpdate({email: req.body.email}, {
+        $set: {
+            'lastLoggedIn' : new Date().toISOString()
+        },
+    },
+    { upsert: true, new: true } 
+    )
 
     res.header('x-auth-token', token).send(`Authentication Successful.`);
 };
---
+++ src/model/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    username: String,
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+    joinedAt: Date,
+    lastLoggedIn: Date,
+    phone: String,
+    address: {
+        type: Schema.Types.ObjectId,
+        ref: 'Address'
+    }
+})
+
+const User = mongoose.model("User", UserSchema)
+module.exports = User
