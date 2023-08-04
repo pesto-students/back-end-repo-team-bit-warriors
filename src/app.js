fda0205c6f2fd23133749eeb0b83393c1745edb2
Mamta Gupta <mamtag962000@gmail.com>
2023-08-05 02:10:46 +0530

    Test modified

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended : false}))
 app.use(express.json());
 dbConnect();
 
-app.get('/', (req, res) => {    
+app.get('/test', (req, res) => {    
     res.status(200).send("Namaste sabhiko !")
 })
 
