a8ec0e6dbd47f5277f76921f24bb53370d98330d
Mamta Gupta <mamtag962000@gmail.com>
2023-08-13 12:35:20 +0530

    Contact us page added

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const userRoutes = require('./routes/userRoutes')
 const adminRoutes = require('./routes/adminRoutes')
 const mallRoutes = require('./routes/mallRoutes')
 const storeRoutes = require('./routes/storeRoutes')
+const connectRoutes = require('./routes/connectRoutes')
 
 const app = express();
 
@@ -19,12 +20,13 @@ dbConnect();
 
 app.get('/test', (req, res) => {    
     res.status(200).send("Namaste sabhiko !")
-})
+});
 
 app.use('/', authRoutes)
 app.use('/users', userRoutes)
 app.use('/admin', adminRoutes)
 app.use('/malls', mallRoutes)
 app.use('/stores', storeRoutes)
+app.use('/contact', connectRoutes)
 
 module.exports = app
