0b74a1bebba665058bb577eb8fa61e465bb9df18
hirenjethva <hiren.j@somaiya.edu>
2023-08-17 21:43:01 +0530

    log removed

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 function auth(req, res, next) {
     // const token = req.header('x-auth-token');
     const token = req.cookies.authCookie;
-    console.log(token);
     if(!token) return res.status(401).send('Access denied. No token provided.');
 
     try{
