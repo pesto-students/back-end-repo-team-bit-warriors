f9aaaf42ac6adc106c34bbf1fc4785e36ebdbb8a
hirenjethva <hiren.j@somaiya.edu>
2023-08-18 16:57:13 +0530

    cors changes

--- src/app.js
+++ src/app.js
@@ -21,29 +21,10 @@ const app = express();
 //     origin: 'https://malldekho.onrender.com',
 //     credentials: true
 // }
-//app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', "https://malldekho.onrender.com");
-  
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization, Access-Control-Allow-Origin');
-  
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
-    // Pass to next layer of middleware
-    next();
-  }
-);
+app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(express.json());
-app.use(cookieParser());
 dbConnect();
 
 app.get('/test', (req, res) => {    
