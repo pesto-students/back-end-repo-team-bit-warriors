b78896e24bd4f79171913c688aa664b3803cd41d
hirenjethva <hiren.j@somaiya.edu>
2023-08-18 16:15:32 +0530

    set cors details

--- src/app.js
+++ src/app.js
@@ -17,11 +17,30 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 // Use cors middleware
-const corsOptions = {
-    origin: 'https://malldekho.onrender.com',
-    credentials: true
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: 'https://malldekho.onrender.com',
+//     credentials: true
+// }
+//app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', "http://localhost:3001");
+  
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization, Access-Control-Allow-Origin');
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+    // Pass to next layer of middleware
+    next();
+  }
+);
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(express.json());
 app.use(cookieParser());
